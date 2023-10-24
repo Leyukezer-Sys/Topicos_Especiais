46c4dc0dbc652561c45e8a89ae70d4fd0df4235c
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2023-10-23 23:39:14 -0400

    ok

--- Aula28082023/api-contatos/src/routes/base.js
+++ Aula28082023/api-contatos/src/routes/base.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Mustache = require('mustache');
+
+const view = {
+    title: "API",
+    descricao: "Funcionando!"
+};
 
 //rotas
 router.get('/', function (req, res) {
-    return res.send('API Funcionando...')
+    return res.send(Mustache.render("<center><h1>{{title}}</h1><br>{{descricao}}<center>", view))
 })
 
 module.exports = router;
